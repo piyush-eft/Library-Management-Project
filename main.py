6344d9e1bf7a7ee4dbc11521e53dd26128e36a3d
Piyush <prpiyushrawat001@gmail.com>
2026-08-19 21:04:30 +0530

    Added Json file and some starting Intro code lines

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+import string
+import random
+from datetime import datetime
+
+
+class Library:
+
+    def add_book(self):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("="*50)
+print("Library Management System")
+print("="*50)
+print("1. Add Book")
+print("2. List Book")
+print("3. Add Member")
+print("4. List Member")
+print("5. Borrow Book")
+print("6. Return Book")
+print("0. Exit Portal")
+print("-"*50)
+
+choice = int(input("Please tell which task you want to perform"))
+
